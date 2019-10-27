6f6a52c5a858c28b4f9a02b4e7ba7d6a2b494481
root <root@Device.mit.edu>
2019-10-26 22:24:44 -0400

    readded CLI.ASSEMBLY

--- src/edu/mit/compilers/codegen/Codegen.java
+++ src/edu/mit/compilers/codegen/Codegen.java
@@ -1,13 +1,25 @@
 package edu.mit.compilers.codegen;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
+import edu.mit.compilers.codegen.ControlFlow.CFAssignment;
+import edu.mit.compilers.codegen.ControlFlow.CFBranch;
+import edu.mit.compilers.codegen.ControlFlow.CFEndMethod;
+import edu.mit.compilers.codegen.ControlFlow.CFMethodCall;
+import edu.mit.compilers.codegen.ControlFlow.CFNop;
+import edu.mit.compilers.codegen.ControlFlow.CFPopScope;
+import edu.mit.compilers.codegen.ControlFlow.CFPushScope;
+import edu.mit.compilers.codegen.ControlFlow.CFShortCircuit;
+import edu.mit.compilers.codegen.ControlFlow.CFStatement;
 import edu.mit.compilers.semantics.IR;
+import edu.mit.compilers.semantics.IR.Op;
 
 public class Codegen {
 	//TODO: there could be name conflicts if something is called "label" or "globalvar"
@@ -15,7 +27,7 @@ public class Codegen {
 	public List<Asm> asmOutput;
 	public static class Asm {
 		public enum Op { //newline is just whitespace for formatting
-			label, pushq, movq, popq, add, sub, ret, custom, newline, xor;
+			label, pushq, movq, popq, add, sub, ret, custom, newline, xor, jz, jnz, test, inc, dec;
 		}
 		public Op op;
 		public String arg1, arg2;
@@ -46,11 +58,11 @@ public class Codegen {
 	public Codegen(ControlFlow CF) {
 		this.CF = CF;
 	}
-	private List<ControlFlow.CFPushScope> scopes;
+	private List<CFPushScope> scopes;
 	private long labelCount;
-	private Map<ControlFlow.CFStatement, String> labels;
+	private Map<CFStatement, String> labels;
 	private Set<String> usedLabels;
-	private void addLabel(ControlFlow.CFStatement _CFS) {
+	private void addLabel(CFStatement _CFS) {
 		if(!labels.containsKey(_CFS)) {
 			String label = "label" + labelCount;
 			labels.put(_CFS, label);
@@ -59,7 +71,7 @@ public class Codegen {
 		}
 		else asmOutput.add(new Asm(Asm.Op.label, labels.get(_CFS))); //a label's already been created for this
 	}
-	private void addLabelIfNonexistent(ControlFlow.CFStatement _CFS) {
+	private void addLabelIfNonexistent(CFStatement _CFS) {
 		if(!labels.containsKey(_CFS)) {
 			String label = "label" + labelCount;
 			labels.put(_CFS, label);
@@ -75,109 +87,125 @@ public class Codegen {
 		}
 		//it's in the global scope
 		if(!CF.program.variables.containsKey(name))
-			throw new IllegalStateException("Variable with name " + name + " not found in any scope");
+			throw new IllegalStateException("Variable with name \"" + name + "\" not found in any scope");
 		return CF.program.variables.get(name).stackOffset + "(%globalvar)";
 	}
-	private void processCFBranch(ControlFlow.CFStatement _CFS) {
-		if(!(_CFS instanceof ControlFlow.CFBranch))
-			return;
-		addLabel(_CFS);
-		ControlFlow.CFBranch CFS = (ControlFlow.CFBranch)_CFS;
-		addLabelIfNonexistent(CFS.next);
-		addLabelIfNonexistent(CFS.next2);
-		processCFBranch(CFS.next);
-		processCFBranch(CFS.next2);
-	}
-	private void processCFS(ControlFlow.CFStatement _CFS) {
-		while(_CFS != null) {
+	private void processCFS(CFStatement _CFS, CFStatement stop) {
+		while(_CFS != null && _CFS != stop) {
 			addLabel(_CFS);
-			if(_CFS instanceof ControlFlow.CFPushScope) { //or Method
-				ControlFlow.CFPushScope CFS = (ControlFlow.CFPushScope)_CFS;
+			if(_CFS instanceof CFPushScope) { //or Method
+				CFPushScope CFS = (CFPushScope)_CFS;
 				scopes.add(CFS);
 				asmOutput.add(new Asm(Asm.Op.pushq, "%rbp"));
 				asmOutput.add(new Asm(Asm.Op.movq, "%rsp", "%rbp"));
 				asmOutput.add(new Asm(Asm.Op.add, "%rsp", "$-" + CFS.stackOffset));
 				_CFS = _CFS.next;
 			}
-			else if(_CFS instanceof ControlFlow.CFPopScope) { //or CFEndMethod
+			else if(_CFS instanceof CFPopScope) { //or CFEndMethod
 				scopes.remove(scopes.size()-1);
 				asmOutput.add(new Asm(Asm.Op.movq, "%rbp", "%rsp"));
 				asmOutput.add(new Asm(Asm.Op.popq, "%rbp"));
-				if(_CFS instanceof ControlFlow.CFEndMethod)
+				if(_CFS instanceof CFEndMethod)
 					asmOutput.add(new Asm(Asm.Op.ret));
 				_CFS = _CFS.next;
 			}
-			else if(_CFS instanceof ControlFlow.CFBranch) {
-				ControlFlow.CFBranch CFS = (ControlFlow.CFBranch)_CFS;
-				
-				addLabelIfNonexistent(CFS.next);
-				addLabelIfNonexistent(CFS.next2);
-				processCFBranch(CFS.next);
-				processCFBranch(CFS.next2);
-			}
-			else if(_CFS instanceof ControlFlow.CFAssignment) {
-				ControlFlow.CFAssignment CFS = (ControlFlow.CFAssignment)_CFS;
-				String loc1 = getVarLoc(CFS.loc.ID);
-				IR.Node mem0 = CFS.expr.members.get(0);
-				if(mem0 instanceof IR.Location)
-				{
-					String loc2 = getVarLoc(((IR.Location)mem0).ID);
-					if(CFS.op.type == IR.Op.Type.assign)
-						asmOutput.add(new Asm(Asm.Op.movq, loc2, loc1));
-					else if(CFS.op.type == IR.Op.Type.plusequals)
-						asmOutput.add(new Asm(Asm.Op.add, loc2, loc1));
-					else if(CFS.op.type == IR.Op.Type.minusequals)
-						asmOutput.add(new Asm(Asm.Op.sub, loc2, loc1));
-					else throw new IllegalStateException("Unexpected op type: " + CFS.op.type.name());
+			else if(_CFS instanceof CFShortCircuit) {
+				CFShortCircuit CFS = (CFShortCircuit)_CFS;
+				Queue<CFStatement> statements = new ArrayDeque<>();
+				statements.add(CFS.start);
+				boolean nextSet = false;
+				while(!statements.isEmpty()) {
+					CFStatement cur = statements.poll();
+					if(cur instanceof CFBranch) {
+						CFBranch CFB = (CFBranch)cur;
+						asmOutput.add(new Asm(Asm.Op.test, ""));
+						asmOutput.add(new Asm(Asm.Op.jnz, ""));
+						statements.add(CFB.next);
+						statements.add(CFB.next2);
+					}
+					else if(cur instanceof CFPushScope) {
+						if(_CFS!=cur && nextSet)
+							throw new IllegalStateException("CFShortCircuit has more than one next CFPushScope");
+						_CFS = cur;
+						nextSet = true;
+					}
 				}
-				else
-				{
-					//TODO: handle multiply nested expr e.g. ((x))
-					
-					if(CFS.expr.members.size() == 1) {
-						if(mem0 instanceof IR.MethodCall) {
-							
-						}
-						else if(mem0 instanceof IR.Len) {
-							
-						}
-						else if(mem0 instanceof IR.IntLiteral) {
-							
-						}
-						else if(mem0 instanceof IR.CharLiteral) {
-							
-						}
-						else if(mem0 instanceof IR.BoolLiteral) {
-							
-						}
-						else throw new IllegalStateException("Unexpected expr member: " + mem0.getClass().getCanonicalName());
+				if(!nextSet)
+					throw new IllegalStateException("CFShortCircuit has no next CFPushScope");
+				List<CFPushScope> oldScopes = new ArrayList<>(scopes);
+				processCFS(_CFS, ((CFPushScope)_CFS).end);
+				scopes = oldScopes;
+				_CFS = _CFS.next;
+			}
+			else if(_CFS instanceof CFAssignment) {
+				CFAssignment CFS = (CFAssignment)_CFS;
+				String loc1 = getVarLoc(CFS.loc.ID);
+				if(CFS.op.type == IR.Op.Type.increment)
+					asmOutput.add(new Asm(Asm.Op.inc, loc1));
+				else if(CFS.op.type == IR.Op.Type.decrement)
+					asmOutput.add(new Asm(Asm.Op.dec, loc1));
+				else {
+					IR.Node mem0 = CFS.expr.members.get(0);
+					if(mem0 instanceof IR.Location)
+					{
+						String loc2 = getVarLoc(((IR.Location)mem0).ID);
+						if(CFS.op.type == IR.Op.Type.assign)
+							asmOutput.add(new Asm(Asm.Op.movq, loc2, loc1));
+						else if(CFS.op.type == IR.Op.Type.plusequals)
+							asmOutput.add(new Asm(Asm.Op.add, loc2, loc1));
+						else if(CFS.op.type == IR.Op.Type.minusequals)
+							asmOutput.add(new Asm(Asm.Op.sub, loc2, loc1));
+						else throw new IllegalStateException("Unexpected op type: " + CFS.op.type.name());
 					}
-					else if(CFS.expr.members.size() == 2) {
-						IR.Node mem1 = CFS.expr.members.get(1);
-						if(!(mem1 instanceof IR.Location))
-							throw new IllegalStateException("Expected IR.Location, got " + mem1.getClass().getCanonicalName());
-						if(mem0 instanceof IR.Op) {
-							if(((IR.Op)mem0).type == IR.Op.Type.not) {
-								if(mem1 instanceof IR.LocationNoArray)
-									asmOutput.add(new Asm(Asm.Op.xor, "$1", getVarLoc(((IR.LocationNoArray)mem1).ID)));
-								else {} //TODO: handle arrays
-							}
-							else throw new IllegalStateException("Unexpected op type: " + ((IR.Op)mem0).type.name());
-						}
-						else throw new IllegalStateException("Unexpected expr member: " + mem0.getClass().getCanonicalName());
-					}
-					else if(CFS.expr.members.size() == 3) {
-						IR.Node mem1 = CFS.expr.members.get(1);
-						if(!(mem1 instanceof IR.Op))
-							throw new IllegalStateException("Expected IR.Op, got " + mem1.getClass().getCanonicalName());
+					else
+					{
+						//TODO: handle multiply nested expr e.g. ((x))
 						
+						if(CFS.expr.members.size() == 1) {
+							if(mem0 instanceof IR.MethodCall) {
+								
+							}
+							else if(mem0 instanceof IR.Len) {
+								
+							}
+							else if(mem0 instanceof IR.IntLiteral) {
+								
+							}
+							else if(mem0 instanceof IR.CharLiteral) {
+								
+							}
+							else if(mem0 instanceof IR.BoolLiteral) {
+								
+							}
+							else throw new IllegalStateException("Unexpected expr member: " + mem0.getClass().getCanonicalName());
+						}
+						else if(CFS.expr.members.size() == 2) {
+							IR.Node mem1 = CFS.expr.members.get(1);
+							if(!(mem1 instanceof IR.Location))
+								throw new IllegalStateException("Expected IR.Location, got " + mem1.getClass().getCanonicalName());
+							if(mem0 instanceof IR.Op) {
+								if(((IR.Op)mem0).type == IR.Op.Type.not) {
+									if(mem1 instanceof IR.LocationNoArray)
+										asmOutput.add(new Asm(Asm.Op.xor, "$1", getVarLoc(((IR.LocationNoArray)mem1).ID)));
+									else {} //TODO: handle arrays
+								}
+								else throw new IllegalStateException("Unexpected op type: " + ((IR.Op)mem0).type.name());
+							}
+							else throw new IllegalStateException("Unexpected expr member: " + mem0.getClass().getCanonicalName());
+						}
+						else if(CFS.expr.members.size() == 3) {
+							IR.Node mem1 = CFS.expr.members.get(1);
+							if(!(mem1 instanceof IR.Op))
+								throw new IllegalStateException("Expected IR.Op, got " + mem1.getClass().getCanonicalName());
+							
+						}
+						else throw new IllegalStateException("Bad expr size: " + CFS.expr.members.size());
 					}
-					else throw new IllegalStateException("Bad expr size: " + CFS.expr.members.size());
 				}
 				_CFS = _CFS.next;
 			}
-			else if(_CFS instanceof ControlFlow.CFMethodCall) {
-				ControlFlow.CFMethodCall CFS = (ControlFlow.CFMethodCall)_CFS;
+			else if(_CFS instanceof CFMethodCall) {
+				CFMethodCall CFS = (CFMethodCall)_CFS;
 				if(CF.importMethods.containsKey(CFS.ID)) {
 					
 				}
@@ -187,6 +215,13 @@ public class Codegen {
 				else throw new IllegalStateException("Unknown method ID: " + CFS.ID);
 				_CFS = _CFS.next;
 			}
+			else if(_CFS instanceof CFNop) {
+				_CFS = _CFS.next;
+			}
+			else throw new IllegalStateException("Unexpected CFS type: " + _CFS.getClass().getCanonicalName());
+			for(int i=0; i<_CFS.scope; i++)
+				System.out.print("  ");
+			System.out.println(_CFS.getClass().getCanonicalName());
 		}
 	}
 	public void build() {
@@ -203,7 +238,7 @@ public class Codegen {
 			if(name.equals("main"))
 				asmOutput.add(new Asm(Asm.Op.custom, ".globl main"));
 			asmOutput.add(new Asm(Asm.Op.label, name));
-			processCFS(method.getValue().code);
+			processCFS(method.getValue().code, null);
 		}
 		
 		asmOutput.removeIf(x -> x.op==Asm.Op.label && !usedLabels.contains(x.arg1));
--- src/edu/mit/compilers/codegen/ControlFlow.java
+++ src/edu/mit/compilers/codegen/ControlFlow.java
@@ -41,6 +41,7 @@ public class ControlFlow {
 	public Map<String, ImportMethodSym> importMethods;
 	public class Program extends CFPushScope {
 		public Program(IR.Node _node) {
+			super(0);
 			IR.Program node = (IR.Program)_node;
 			for(IR.ImportDecl i: node.imports)
 				if(i instanceof IR.ImportDecl)
@@ -52,80 +53,104 @@ public class ControlFlow {
 	}
 	public class Method extends CFPushScope {
 		public Method(IR.Node _node) {
+			super(1);
 			IR.MethodDecl node = (IR.MethodDecl)_node;
 			for(IR.MethodDeclParam i: node.params) {
 				variables.put(i.ID, new VarSym(i.type, 0, stackOffset));
 				stackOffset += wordSize;
 			}
-			end = new CFEndMethod();
-			next = makeBlock(node.block, end);
+			end = new CFEndMethod(1);
+			next = makeBlock(node.block, 2, end, null, null);
 		}
 	}
-	public CFStatement makeBlock(IR.Block block, CFStatement endBlock) {
-		CFPushScope pushScope = new CFPushScope();
+	public CFStatement makeBlock(IR.Block block, int scope, CFStatement endBlock, CFStatement breakCFS, CFStatement continueCFS) {
+		CFPushScope pushScope = new CFPushScope(scope);
 		CFStatement start = pushScope;
 		pushScope.addFields(block.fields);
 		CFStatement cur = pushScope;
 		for(IR.Statement i: block.statements) {
 			if(i instanceof IR.AssignmentStatement) {
-				cur.next = new CFAssignment((IR.AssignmentStatement)i);
+				cur.next = new CFAssignment(scope, (IR.AssignmentStatement)i);
 				cur = cur.next;
 			}
 			else if(i instanceof IR.IfStatement) {
-				CFMergeBranch end = new CFMergeBranch();
+				CFMergeBranch end = new CFMergeBranch(scope);
 				IR.IfStatement ifS = (IR.IfStatement)i;
-				CFShortCircuit sC = new CFShortCircuit();
+				CFShortCircuit sC = new CFShortCircuit(scope);
 				if(ifS.elseBlock == null)
-					sC.start = shortCircuit(ifS.condition, makeBlock(ifS.block, end), end);
-				else sC.start = shortCircuit(ifS.condition, makeBlock(ifS.block, end), makeBlock(ifS.elseBlock, end));
+					sC.start = shortCircuit(ifS.condition, scope, makeBlock(ifS.block, scope+1, end, breakCFS, continueCFS), end);
+				else sC.start = shortCircuit(ifS.condition, scope, makeBlock(ifS.block, scope+1, end, breakCFS, continueCFS),
+								makeBlock(ifS.elseBlock, scope+1, end, breakCFS, continueCFS));
 				cur.next = sC;
 				cur = end;
 			}
 			else if(i instanceof IR.WhileStatement) {
-				cur.next = new CFNop();
+				cur.next = new CFNop(scope);
 				cur = cur.next;
-				CFMergeBranch end = new CFMergeBranch();
+				CFMergeBranch end = new CFMergeBranch(scope);
 				IR.WhileStatement wS = (IR.WhileStatement)i;
-				CFBranch whileBranch = shortCircuit(wS.condition, makeBlock(wS.block, cur), end);
-				cur.next = whileBranch;
+				CFShortCircuit sC = new CFShortCircuit(scope);
+				sC.start = shortCircuit(wS.condition, scope, makeBlock(wS.block, scope+1, cur, end, cur), end);
+				cur.next = sC;
 				cur = end;
 			}
 			else if(i instanceof IR.ForStatement) {
 				IR.ForStatement fS = (IR.ForStatement)i;
-				cur.next = new CFAssignment(fS.initLoc, fS.initExpr);
+				cur.next = new CFAssignment(scope, fS.initLoc, fS.initExpr);
 				cur = cur.next;
-				cur.next = new CFNop();
+				cur.next = new CFNop(scope);
 				cur = cur.next;
-				CFMergeBranch end = new CFMergeBranch();
-				CFAssignment iteration = new CFAssignment(fS.iteration);
-				iteration.next = end;
-				CFBranch forBranch = shortCircuit(fS.condition, makeBlock(fS.block, iteration), end);
-				cur.next = forBranch;
+				CFMergeBranch end = new CFMergeBranch(scope);
+				CFAssignment iteration = new CFForAssignment(scope+1, fS.iteration);
+				iteration.next = cur;
+				CFShortCircuit sC = new CFShortCircuit(scope);
+				sC.start = shortCircuit(fS.condition, scope, makeBlock(fS.block, scope+1, iteration, end, iteration), end);
+				cur.next = sC;
 				cur = end;
 			}
 			else if(i instanceof IR.BreakStatement) {
-				break; //exit the current scope
+				pushScope.end = cur.next = new CFPopScope(scope);
+				cur = cur.next;
+				int parentScope = 2;
+				if(breakCFS != null)
+					parentScope = breakCFS.scope;
+				for(int j=scope-1; j>parentScope; j--) {
+					cur.next = new CFPopScope(scope);
+					cur = cur.next;
+				}
+				cur.next = endBlock;
+				return start;
 			}
 			else if(i instanceof IR.ContinueStatement) {
-				cur.next = start; //TODO: I think this works
+				pushScope.end = cur.next = new CFPopScope(scope);
+				cur = cur.next;
+				int parentScope = 2;
+				if(continueCFS != null)
+					parentScope = continueCFS.scope;
+				for(int j=scope-1; j>parentScope; j--) {
+					cur.next = new CFPopScope(scope);
+					cur = cur.next;
+				}
+				cur.next = endBlock;
+				return start;
 			}
 			else if(i instanceof IR.ReturnStatement) {
-				cur.next = new CFReturn((IR.ReturnStatement)i);
+				cur.next = new CFReturn(scope, (IR.ReturnStatement)i);
 				cur = cur.next;
 				break;
 			}
 			else if(i instanceof IR.MethodCall) {
-				cur.next = new CFMethodCall((IR.MethodCall)i);
+				cur.next = new CFMethodCall(scope, (IR.MethodCall)i);
 				cur = cur.next;
 			}
 			else throw new IllegalStateException("Bad statement class " + i.getClass().getCanonicalName());
 		}
-		pushScope.end = cur.next = new CFPopScope();
+		pushScope.end = cur.next = new CFPopScope(scope);
 		cur.next.next = endBlock;
 		return start;
 	}
-	public CFBranch shortCircuit(IR.Expr condition, CFStatement ifTrue, CFStatement ifFalse) {
-		CFBranch branch = new CFBranch(condition);
+	public CFBranch shortCircuit(IR.Expr condition, int scope, CFStatement ifTrue, CFStatement ifFalse) {
+		CFBranch branch = new CFBranch(scope, condition);
 		if(condition.members.size() == 1) {
 			branch.next = ifTrue;
 			branch.next2 = ifFalse;
@@ -136,7 +161,7 @@ public class ControlFlow {
 				IR.Op op = (IR.Op)node;
 				if(op.type != IR.Op.Type.not)
 					throw new IllegalStateException("Expected Op of type not");
-				branch = shortCircuit((IR.Expr)condition.members.get(1), ifFalse, ifTrue); //guaranteed to be IR.Expr by grammar (I think)
+				branch = shortCircuit((IR.Expr)condition.members.get(1), scope, ifFalse, ifTrue); //guaranteed to be IR.Expr by grammar (I think)
 			}
 			else throw new IllegalStateException("Unrecognized Op");
 		}
@@ -145,11 +170,11 @@ public class ControlFlow {
 			if(nOp instanceof IR.Op) {
 				IR.Op op = (IR.Op)nOp;
 				if(op.type == IR.Op.Type.andand)
-					branch = shortCircuit((IR.Expr)condition.members.get(0),
-							 shortCircuit((IR.Expr)condition.members.get(2), ifTrue, ifFalse), ifFalse);
+					branch = shortCircuit((IR.Expr)condition.members.get(0), scope,
+							 shortCircuit((IR.Expr)condition.members.get(2), scope, ifTrue, ifFalse), ifFalse);
 				else if(op.type == IR.Op.Type.oror)
-					branch = shortCircuit((IR.Expr)condition.members.get(0),
-							 ifTrue, shortCircuit((IR.Expr)condition.members.get(2), ifTrue, ifFalse));
+					branch = shortCircuit((IR.Expr)condition.members.get(0), scope,
+							 ifTrue, shortCircuit((IR.Expr)condition.members.get(2), scope, ifTrue, ifFalse));
 				else {
 					branch.next = ifTrue;
 					branch.next2 = ifFalse;
@@ -161,25 +186,35 @@ public class ControlFlow {
 		return branch;
 	}
 	public abstract class CFStatement {
-		public IR.Expr condition;
-		public CFStatement next, next2;
-		public CFStatement() {}
+		public CFStatement next;
+		public int scope;
+		public CFStatement(int scope) {
+			this.scope = scope;
+		}
 	}
 	public class CFNop extends CFStatement {
-		
+		public CFNop(int scope) {
+			super(scope);
+		}
 	}
 	public class CFMergeBranch extends CFNop {
-		
+		public CFMergeBranch(int scope) {
+			super(scope);
+		}
 	}
 	public class CFShortCircuit extends CFStatement {
 		public CFStatement start;
+		public CFShortCircuit(int scope) {
+			super(scope);
+		}
 	}
 	public class CFPushScope extends CFStatement {
 		public CFPushScope parent;
 		public CFStatement end;
 		public Map<String, VarSym> variables;
 		long stackOffset;
-		public CFPushScope() {
+		public CFPushScope(int scope) {
+			super(scope);
 			variables = new HashMap<>();
 			stackOffset = 0;
 		}
@@ -199,41 +234,60 @@ public class ControlFlow {
 		}
 	}
 	public class CFPopScope extends CFStatement {
-		
+		public CFPopScope(int scope) {
+			super(scope);
+		}
 	}
 	public class CFEndMethod extends CFPopScope {
-		
+		public CFEndMethod(int scope) {
+			super(scope);
+		}
 	}
 	public class CFAssignment extends CFStatement {
 		public IR.Location loc;
 		public IR.Op op;
 		public IR.Expr expr;
-		public CFAssignment(IR.AssignmentStatement node) {
-			loc = node.loc;
-			op = node.op;
-			expr = node.assignExpr;
+		public CFAssignment(int scope, IR.AssignmentStatement node) {
+			super(scope);
+			this.loc = node.loc;
+			this.op = node.op;
+			this.expr = node.assignExpr;
 		}
-		public CFAssignment(IR.Location loc, IR.Expr expr) {
+		public CFAssignment(int scope, IR.Location loc, IR.Expr expr) {
+			super(scope);
 			this.loc = loc;
 			this.op = new IR.Op(null, -1, IR.Op.Type.assign); //TODO: this has a null parent. is null parent ok? probably
 			this.expr = expr;
 		}
 	}
+	public class CFForAssignment extends CFAssignment {
+		public CFForAssignment(int scope, IR.AssignmentStatement node) {
+			super(scope, node);
+		}
+		public CFForAssignment(int scope, IR.Location loc, IR.Expr expr) {
+			super(scope, loc, expr);
+		}
+	}
 	public class CFBranch extends CFStatement {
-		public CFBranch(IR.Expr condition) {
+		public IR.Expr condition;
+		public CFStatement next2;
+		public CFBranch(int scope, IR.Expr condition) {
+			super(scope);
 			this.condition = condition;
 		}
 	}
 	public class CFReturn extends CFStatement {
 		public IR.Expr expr;
-		public CFReturn(IR.ReturnStatement node) {
+		public CFReturn(int scope, IR.ReturnStatement node) {
+			super(scope);
 			this.expr = node.expr;
 		}
 	}
 	public class CFMethodCall extends CFStatement {
 		public String ID;
 		public List<Object> params;
-		public CFMethodCall(IR.MethodCall call) {
+		public CFMethodCall(int scope, IR.MethodCall call) {
+			super(scope);
 			this.ID = call.ID;
 			this.params = new ArrayList<>();
 			for(IR.MethodParam i: call.params)
